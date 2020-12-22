27e1821d814bd8ce39a460854d79c709bd5ca711
Alex Anderson <alexandersonone@gmail.com>
2020-12-22 12:56:19 -0800

    Add the filters as a feature

--- main.go
+++ main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/appins/csvcmd/pkg/csvfilter"
 	"github.com/appins/csvcmd/pkg/csvtrunc"
 )
+
+type options struct {
+	humanReadable bool
+	startLine     int
+	endLine       int
+	filtersString string
+}
 
 func main() {
 	var humanReadable bool
 	flag.BoolVar(&humanReadable, "h", false, "Print in an easy to read format")
 
 	var startLine int
-	flag.IntVar(&startLine, "s", 0, "The first line that should be read (inclusive)")
+	flag.IntVar(&startLine, "s", 0, "The first line, after the initial column line, that should be read (inclusive, 1-based index)")
 
 	var endLine int
-	flag.IntVar(&endLine, "e", -1, "The last line that should be read (inclusive)")
+	flag.IntVar(&endLine, "e", -1, "The last line, after the initial column line, that should be read (inclusive, 1-based index)")
+
+	var filtersString string
+	flag.StringVar(&filtersString, "f", "", "Filters on columns, see GitHub for examples")
 
 	flag.Parse()
+
+	opts := options{humanReadable, startLine, endLine, filtersString}
 
 	// The files that should be read
 	if len(flag.Args()) != 0 {
@@ -30,13 +44,60 @@ func main() {
 
 	} else {
 		// In the case of no files being specified, read from stdin
-		csvReader, _ := csvtrunc.NewReader(os.Stdin, startLine, endLine)
-		filters := []func([]string) bool{}
-		filteredReader := csvfilter.NewReader(csvReader, filters, false)
-		for filteredReader.Scan() {
-			fmt.Println(filteredReader.Row())
+		csvReader, cols, _ := csvtrunc.NewReader(os.Stdin, startLine, endLine)
+		processFile(csvReader, cols, opts)
+	}
+
+}
+
+func genFilters(filterString string, cols []string) ([]func([]string) bool, error) {
+	var filters []func([]string) bool
+	// Create a map of all columns, so that we can search for them
+	colsToInt := make(map[string]int)
+	for i, col := range cols {
+		colsToInt[col] = i
+	}
+
+	// Break the filterString into indivisual filters
+	filterStrings := strings.Split(filterString, ";")
+	for _, filter := range filterStrings {
+		// Skip empty filters
+		if len(filter) == 0 {
+			continue
 		}
 
+		// Break apart the filter by =. If there is an equal sign, it's an = filter
+		if parts := strings.Split(filter, "="); len(parts) == 2 {
+			// We gotta check if the column exists. If it does, we can refer to it as col
+			if col, ok := colsToInt[parts[0]]; ok {
+				filters = append(filters, func(row []string) bool {
+					return row[col] == parts[1]
+				})
+			} else {
+				// No col -> fail to parse all of the filters
+				// IDEA: have some character that can ignore faulty columns
+				return filters, errors.New("Couldn't find column " + parts[0])
+			}
+
+		} else {
+			return filters, errors.New("Couldn't parse filter " + filter)
+		}
+	}
+
+	return filters, nil
+
+}
+
+func processFile(csvReader *csvtrunc.Reader, cols []string, opts options) {
+
+	filters, err := genFilters(opts.filtersString, cols)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	filteredReader := csvfilter.NewReader(csvReader, filters, false)
+	for filteredReader.Scan() {
+		fmt.Println(filteredReader.Row())
 	}
 
 }
--- pkg/csvtrunc/csvtrunc.go
+++ pkg/csvtrunc/csvtrunc.go
@@ -15,24 +15,31 @@ type Reader struct {
 
 // NewReader creates a new object that automatically discards lines until start
 // and only reads up until the stop line (inclusive). If stop is <= 0, we don't
-// use it to calculate a stopping point
-func NewReader(r io.Reader, start int, stop int) (*Reader, error) {
+// use it to calculate a stopping point. Note that this is 1-indexed, but only
+// if you consider the first row (the column names) the 0th index. Note that the
+// first row is returned, so that column names can be known.
+func NewReader(r io.Reader, start int, stop int) (*Reader, []string, error) {
 
 	// Create a csv.Reader
 	csvReader := csv.NewReader(r)
+	// Read the first row, which are the column names
+	cols, err := csvReader.Read()
+	if err != nil {
+		return nil, cols, err
+	}
 
-	// Discard the first start-1 lines
+	// Discard the first start-1 lines (start including
 	for i := 1; i < start; i++ {
 		_, err := csvReader.Read()
 		// there shouldn't be any errors reading the first start lines
 		if err != nil {
-			return nil, err
+			return nil, cols, err
 		}
 
 	}
 
 	// Note that we read start-1 lines, so we're at line start
-	return &Reader{csvReader, start, stop}, nil
+	return &Reader{csvReader, start, stop}, cols, nil
 }
 
 // Read reads a line from the csv. It checks to make sure that the line is before end
